8a86c5743873b5b85741b2cf11cc8c5885ab4d4f
Jarek S <jarek.sygitowicz@authologic.com>
2022-02-04 19:39:32 +0100

    initial index styling

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "navy": "#272D73",
+      },
+      fontFamily: {
+        'outfit': ['Outfit', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
