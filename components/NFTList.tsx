0ea31daafae09f7c50e7911b959e5dbdf5fbe936
Jarek S <jarek.sygitowicz@authologic.com>
2022-02-04 23:29:08 +0100

    fixed mobile styling

--- components/NFTList.tsx
+++ components/NFTList.tsx
@@ -101,7 +101,7 @@ export class NFTList extends React.Component<Props, State> {
 </svg>
           <input className="w-full ml-2 p-2 rounded-md" placeholder="Your email address." type="text" id="email-input"/>
         </div></div>
-        <div className="grid gap-2 grid-cols-4">
+        <div className="grid gap-2 grid-cols-1 md:grid-cols-4">
           {this.state.shouldFetchNFTs ?
             <div className="absolute inset-0 backdrop-blur-md text-slate-300 w-full h-screen bg-navy/80 mx-auto flex items-center justify-center"><div class="animate-bounce mr-1"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
