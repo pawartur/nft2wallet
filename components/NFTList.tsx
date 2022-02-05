3e9bdcc605add62d2fe3e82b81e0625ab32bb3bb
Jarek S <jarek.sygitowicz@authologic.com>
2022-02-05 22:33:33 +0100

    fixed text color for email input

--- components/NFTList.tsx
+++ components/NFTList.tsx
@@ -126,7 +126,7 @@ export class NFTList extends React.Component<Props, State> {
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
 </svg>
           <input 
-            className="w-full ml-2 p-2 rounded-md" 
+            className="w-full ml-2 p-2 rounded-md text-slate-700" 
             placeholder="Your email address." 
             type="text" 
             id="email-input"
