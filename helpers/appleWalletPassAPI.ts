d00c10d25c75099b041f28ea26bac33ed5ca9b29
Artur Wdowiarski <arturwdowiarski@me.com>
2022-02-05 17:48:57 +0000

    Added NFT image as coupon's thumbnail, styled the coupon and deleted leftovers.

--- helpers/appleWalletPassAPI.ts
+++ helpers/appleWalletPassAPI.ts
@@ -1,32 +1,52 @@
 const { Template } = require("@walletpass/pass-js");
+const im = require('imagemagick');
 import path from 'path'
+import fs from 'fs'
 import { NFT, NFTMetaData } from '../@types/types'
 import { normaliseURL } from './urlAPI';
+
+const getResizePromise = (
+  image: any,
+  filepath: string
+  ) => {
+  return new Promise((resolve) => {
+      setTimeout(() => {
+        im.resize({
+          srcData: image,
+          format: 'png',
+          width: 90
+        }, (err: any, stdout: any, stderr: any) => {
+          fs.writeFileSync(filepath, stdout, 'binary');
+          resolve(true)
+        })
+      }, 60);
+  });
+}
 
 export async function generatePass(nft: NFT): Promise<any> {
   // FIXME: Don't store the cert in the repo in the final product ;P 
   const dir = path.resolve('./resources');
 
   let nftName = nft.name
-  // let thumbnail: any | undefined = undefined
+  let image: any | undefined = undefined
   if (nft?.token_uri) {
     const response = await fetch(nft.token_uri, {method: 'GET'})
     const nftMetaData = await response.json()
     nftName = nftMetaData.name
     const imageURL = nftMetaData.image_url || nftMetaData.image
-    // if (imageURL) {
-    //   const response = await fetch(normaliseURL(imageURL));
-    //   const arrayBuffer = await response.arrayBuffer();
-    //   thumbnail = Buffer.from(arrayBuffer);
-    // }
+    if (imageURL) {
+      const response = await fetch(normaliseURL(imageURL));
+      const arrayBuffer = await response.arrayBuffer();
+      image = Buffer.from(arrayBuffer);
+    }
   }
 
   const template = new Template("generic", {
     passTypeIdentifier: "pass.me.arturwdowiarski.first-pass",
     teamIdentifier: "Y23Q74DCEK",
     organizationName : "NFT 2 Wallet",
-    foregroundColor : "rgb(255, 255, 255)",
-    backgroundColor : "rgb(197, 31, 31)",
+    foregroundColor : "rgb(203, 213, 225)",
+    backgroundColor : "rgb(39, 45, 115)",
     description: "NFT 2 Wallet Pass",
     sharingProhibited: true,
     generic: {
@@ -47,9 +67,16 @@ export async function generatePass(nft: NFT): Promise<any> {
   ]
   await template.images.add("icon", "./resources/passes/NFT.pass/icon.png")
   await template.images.add("logo", "./resources/passes/NFT.pass/logo.png")
-  // if (thumbnail) {
-  //   await template.images.add("thumbnail", thumbnail)
-  // }
+  const filepath = "./resources/" + nft.token_address + ".png"
+  if (image) {
+    if (!fs.existsSync(filepath)) {
+      const thumbnail = await getResizePromise(
+        image,
+        filepath
+      )
+    }
+    await template.images.add("thumbnail", filepath)
+  }
 
   await template.loadCertificate("./resources/cert/NFT2WalletSignerCert.pem", "nft2wallet");
   template.serialNumber = nft.token_address
