253a342c10dac49b1262ddfbfea77301f02e2ff5
Artur Wdowiarski <arturwdowiarski@me.com>
2022-02-05 19:15:22 +0000

    Started fixing errors

--- components/NFTList.tsx
+++ components/NFTList.tsx
@@ -77,7 +77,7 @@ export class NFTList extends React.Component<Props, State> {
       <div className="w-full rounded-xl shadow-inner bg-slate-300 mb-6">
       <img className="p-1 rounded-xl" id="nft_artwork" src={this.getNFTImageURL(nft)}></img>
       <div className="flex pt-4 pb-4 pl-2 items-center text-left text-xs text-slate-700 font-semibold font-sans">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
 </svg>{this.state.nftsMetaData[nft.token_address]?.name}
         </div>
@@ -103,8 +103,8 @@ export class NFTList extends React.Component<Props, State> {
         <div className="text-sm p-2 md:p-0 text-center md:text-left">
         Choose the NFT you want to send to your phone via email
         </div>
-        <div class="w-full mb-2 ml-2 p-2 rounded-md flex items-center border bg-white">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <div className="w-full mb-2 ml-2 p-2 rounded-md flex items-center border bg-white">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
 </svg>
           <input className="w-full ml-2 p-2 rounded-md" placeholder="Your email address." type="text" id="email-input"/>
--- pages/verify/[hash].tsx
+++ pages/verify/[hash].tsx
@@ -20,7 +20,7 @@ const { width, height } = useWindowSize()
      
       <main className="p-2 bg-navy w-full min-h-full text-center font-outfit">
       <Confetti
-      numberOfPieces="100"
+      numberOfPieces={100}
       width={width}
       height={height}
     />
@@ -28,7 +28,7 @@ const { width, height } = useWindowSize()
 <div><img src="/img/NFT2walletLogo.png" className="h-36"></img></div>
 
 
-  <div className="text-slate-300 font-sans font-semibold p-3 md:p-4 text-xs md:text-md uppercase flex items-center"> <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <div className="text-slate-300 font-sans font-semibold p-3 md:p-4 text-xs md:text-md uppercase flex items-center"> <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
 </svg> secure pass verification</div>
              
@@ -45,7 +45,7 @@ const { width, height } = useWindowSize()
 
      <div className="w-full p-6 flex items-center justify-center text-slate-300 font-sans font-light">
         
-        <div className=""> Made with</div> <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 p-1 text-red-600" viewBox="0 0 20 20" fill="currentColor">
+        <div className=""> Made with</div> <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 p-1 text-red-600" viewBox="0 0 20 20" fill="currentColor">
  <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
 </svg> for ETHGlobal Hackathon
        
