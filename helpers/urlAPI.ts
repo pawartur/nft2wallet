ec2d02bb4fc773a525f45b9d8591bef895e3652b
Artur Wdowiarski <arturwdowiarski@me.com>
2022-02-06 16:30:47 +0000

    Return an url of a "missing image", if we can't parse image url

--- helpers/urlAPI.ts
+++ helpers/urlAPI.ts
@@ -1,9 +1,11 @@
 export function normaliseURL(url: string): string {
   if (url.startsWith("ipfs")) {
     return "https://ipfs.io/ipfs/"+url.split("ipfs://").slice(-1)
-  } else if (!url.startsWith("data:image")) {
+  } else if (url.startsWith("http") || url.startsWith("https")) {
     return url+"?format=json"
-  } else {
+  } else if (url.startsWith("data:image")) {
     return url
+  } else {
+    return "https://i.ibb.co/4Fqw7b6/missing-Image.png"
   }
 }
