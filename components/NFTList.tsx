475b78b479d240cb74c5327858e661c3e47d7a6a
Jarek S <jarek.sygitowicz@authologic.com>
2022-02-06 15:25:12 +0100

    success info

--- components/NFTList.tsx
+++ components/NFTList.tsx
@@ -187,8 +187,9 @@ export class NFTList extends React.Component<Props, State> {
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
 </svg></div>Your NFT is being transformed into an Apple Wallet Pass. Please wait!</div></div>
-
-        <div className="w-full bg-white" id="action-confirm" style={{display: 'none' }}>This is a wating element/info box after request is completed</div>
+<div className="w-full sticky inset-0 p-10 rounded-xl bg-green-200 font-sans font-semibold flex items-center justify-center" id="action-confirm" style={{display: 'none' }}> <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+</svg>Your Apple Pass was sent successfuly!</div>
       </div>
     );
   }
