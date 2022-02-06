98ff820eccf9ebf6b69b6cc499451c8040d45f9e
Artur Wdowiarski <arturwdowiarski@me.com>
2022-02-06 16:25:05 +0000

    Don't add 'format=json' to normalised image urls when the 'url' starts with 'data:image'

--- helpers/urlAPI.ts
+++ helpers/urlAPI.ts
@@ -1,7 +1,9 @@
 export function normaliseURL(url: string): string {
   if (url.startsWith("ipfs")) {
     return "https://ipfs.io/ipfs/"+url.split("ipfs://").slice(-1)
-  } else {
+  } else if (!url.startsWith("data:image")) {
     return url+"?format=json"
+  } else {
+    return url
   }
 }
