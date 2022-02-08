3baacf31907528e7af6031672dc4cd001d518d3f
Artur Wdowiarski <arturwdowiarski@me.com>
2022-02-08 13:29:47 +0000

    Show failed load state in nft verificator

--- components/NFTVerificator.tsx
+++ components/NFTVerificator.tsx
@@ -34,9 +34,9 @@ export class NFTVerificator extends React.Component<Props, State> {
       this.props.tokenId
     ).then(nft => {
       this.setState({
+        ...this.state,
         isLoadingNFT: false,
-        nft: nft || null,
-        addConfetti: true
+        nft: nft || null
       })
   
       if (nft?.token_uri) {
@@ -45,7 +45,8 @@ export class NFTVerificator extends React.Component<Props, State> {
           response.json().then((nftMetadata: NFTMetaData) => {
             this.setState({
               ...this.state,
-              cachedNFTMetaData: nftMetadata
+              cachedNFTMetaData: nftMetadata,
+              addConfetti: true
             })
           })
         })
@@ -64,7 +65,7 @@ export class NFTVerificator extends React.Component<Props, State> {
       <div>
         {this.state.addConfetti ? 
           <Confetti
-            numberOfPieces={1000}
+            numberOfPieces={300}
             width={this.props.windowWidth}
             height={this.props.windowHeight}
           />
@@ -72,10 +73,10 @@ export class NFTVerificator extends React.Component<Props, State> {
         }
         <div className="w-full min-h-screen text-slate-300 font-sans font-semibold">
           {this.state.isLoadingNFT ? 
-            <div className="w-full flex items-center justify-center"><div className="animate-spin"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <div className="w-full flex items-center justify-center"><div className="animate-spin"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-          </svg></div> Fetching the NFT... </div>
+          </svg></div> Loading NFT from the wallet address... </div>
           :
             this.state.cachedNFTMetaData ? 
               <div className="md:w-1/4 w-full mx-auto p-4 bg-slate-300 rounded-xl">
@@ -84,10 +85,7 @@ export class NFTVerificator extends React.Component<Props, State> {
                 <img src={this.getNFTImageURL()}/>
               </div>
             :
-              <div className="flex items-center"><div className="animate-spin"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-            </svg></div> Loading NFT from the wallet address... </div>
+              <div className="flex items-center">Couldn't fetch the NFT </div>
           }
         </div>
       </div>
